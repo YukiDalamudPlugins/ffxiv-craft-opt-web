dec42d090637ddfb6949b0401f9356c3acecb4da
sheiun <me.sheiun@gmail.com>
2022-11-28 13:58:20 +0800

    Clean html and update role sorting

--- app/js/services/actions.js
+++ app/js/services/actions.js
@@ -2,14 +2,14 @@
     'use strict';
 
     var allClasses = [
-        "Alchemist",
-        "Armorer",
-        "Blacksmith",
         "Carpenter",
-        "Culinarian",
+        "Blacksmith",
+        "Armorer",
         "Goldsmith",
         "Leatherworker",
-        "Weaver"
+        "Weaver",
+        "Alchemist",
+        "Culinarian"
     ];
 
     var extraActionInfo = {
